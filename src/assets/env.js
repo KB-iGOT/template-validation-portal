59db2632e3a07d7708c2a41c530bdecae342c3fa
anudeep-ks <anudeep.ksk@gmail.com>
2024-08-19 16:53:33 +0530

    PR review commit

--- src/assets/env.js
+++ src/assets/env.js
@@ -1,11 +1,8 @@
-// Define the environment configuration
 window["env"] = {
-    customAuth: true,
-    baseUrl: 'http://localhost:5000/template/api/v1/',
-    baseUrls: {
-        qa: 'https://portal.karmayogiqa.nic.in/',
-        dev: 'https://portal.dev.karmayogibharat.net/',
-        bm: 'https://portal.karmayogibm.nic.in/',
-        prod: 'https://igotkarmayogi.gov.in/'
-    }
+  customAuth: true,
+  baseUrl: 'http://localhost:5000/template/api/v1/',
+  qa: 'https://portal.karmayogiqa.nic.in/',
+  dev: 'https://portal.dev.karmayogibharat.net/',
+  bm: 'https://portal.karmayogibm.nic.in/',
+  prod: 'https://igotkarmayogi.gov.in/'
 };
