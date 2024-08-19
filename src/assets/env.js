d00f7acf2c022e330c38df762b2b423eca5ca9cc
anudeep-ks <anudeep.ksk@gmail.com>
2024-08-19 18:19:22 +0530

    Updated PR commit

--- src/assets/env.js
+++ src/assets/env.js
@@ -1,8 +1,4 @@
 window["env"] = {
   customAuth: true,
-  baseUrl: 'http://localhost:5000/template/api/v1/',
-  qa: 'https://portal.karmayogiqa.nic.in/',
-  dev: 'https://portal.dev.karmayogibharat.net/',
-  bm: 'https://portal.karmayogibm.nic.in/',
-  prod: 'https://igotkarmayogi.gov.in/'
+  baseUrl: 'http://localhost:5000/template/api/v1/' 
 };
