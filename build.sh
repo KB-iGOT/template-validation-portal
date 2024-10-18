121e852d6dcf5237a5a4ccc66e60c1316d105f20
Haritest <haridasksd@gmail.com>
2024-10-18 21:12:40 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
+
+echo "Docker build script"
+
 # Build script
 set -eo pipefail
 
 build_tag=$1
-name='solution-upload-portal'
+name=ml-template-validation-backend-service
 node=$2
 org=$3
 
-docker build -f Dockerfile --label commitHash=$(git rev-parse --short HEAD) -t ${org}/${name}:${build_tag} .
+docker build -f ./Dockerfile --label commitHash=$(git rev-parse --short HEAD) -t ${org}/${name}:${build_tag} .
+
 echo {\"image_name\" : \"${name}\", \"image_tag\" : \"${build_tag}\", \"node_name\" : \"$node\"} > metadata.json
