38726d95b7bd48bc5d8030315ea1481e4386f49e
Haritest <haridasksd@gmail.com>
2026-06-10 22:55:22 +0530

    Update build script for UI service

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+!/bin/bash
 
 echo "Docker build script"
 
@@ -6,10 +6,10 @@ echo "Docker build script"
 set -eo pipefail
 
 build_tag=$1
-name=cbp-ai-service
+name='cbp-ai-ui'
 node=$2
 org=$3
-
+ls -al
 docker build -f ./Dockerfile --label commitHash=$(git rev-parse --short HEAD) -t ${org}/${name}:${build_tag} .
 
 echo {\"image_name\" : \"${name}\", \"image_tag\" : \"${build_tag}\", \"node_name\" : \"$node\"} > metadata.json
