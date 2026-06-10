43da5e2983f80b8b34363a7a3222fd42524570e7
Haritest <haridasksd@gmail.com>
2026-06-10 22:50:41 +0530

    testing

--- build.sh
+++ build.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
+
 echo "Docker build script"
+
+# Build script
 set -eo pipefail
+
 build_tag=$1
-name=ai-assessment-worker-service
+name=ai-cbp-mdo-service
 node=$2
 org=$3
+
 docker build -f ./Dockerfile --label commitHash=$(git rev-parse --short HEAD) -t ${org}/${name}:${build_tag} .
+
 echo {\"image_name\" : \"${name}\", \"image_tag\" : \"${build_tag}\", \"node_name\" : \"$node\"} > metadata.json
